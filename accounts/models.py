967a4d009fae72f481f775e56b7be0c32a5759dc
jayanth ns <jayanthnsjava@gmail.com>
2018-07-22 18:37:26 +0530

    fixes

--- accounts/models.py
+++ accounts/models.py
@@ -63,7 +63,7 @@ class Account(AbstractBaseUser):
     lastname = models.CharField(max_length=128, blank=True)
     username = models.CharField(max_length=128, blank=True)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)  # A company HR not Talex Admin
+    is_staff = models.BooleanField(default=False) 
     is_superuser = models.BooleanField(default=False)  # A SUPER USER
     last_login = models.DateTimeField(null=True, blank=True)
     forgot_password_token = models.CharField(max_length=120, null=True, blank=True)
